8fc5f835bc99edd4b37d2cab01c3111333e26209
AR123456 <coloradoz99@gmail.com>
2026-03-19 06:03:13 -0400

    react native style

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,20 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hello World !</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World !</Text>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#25292e",
+  },
+  text: {
+    color: "#fff",
+  },
+});
