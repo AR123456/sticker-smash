5cb7a05ce169880c208b1662ad3f0b917391cbc2
AR123456 <coloradoz99@gmail.com>
2026-03-24 06:04:46 -0400

    background image

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,11 @@
-import { View, StyleSheet } from "react-native";
-import ImageViewer from "@/components/ImageViewer";
-
+import { Text, View, StyleSheet } from "react-native";
+import { Image } from "expo-image";
 const PlaceholderImage = require("../../assets/images/background-image.png");
 export default function Index() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceholderImage} />
+        <Image source={PlaceholderImage} style={styles.image}></Image>
       </View>
     </View>
   );
