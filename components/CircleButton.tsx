6cee5c9c5880a5fbb415b6236c89ec4a897db79e
AR123456 <coloradoz99@gmail.com>
2026-03-27 07:30:21 -0400

    export function

--- components/CircleButton.tsx
+++ components/CircleButton.tsx
@@ -1,2 +1,15 @@
 import { View, Pressable, StyleSheet } from 'react-native';
-import MaterialIcons from '@expo/vector-icons/MaterialIcons
+import MaterialIcons from '@expo/vector-icons/MaterialIcons;
+
+type Props = {
+  onPress: () => void;
+};
+export default function CircleButton({ onPress }: Props) {
+  return (
+    <View style={styles.circleButtonContainer}>
+      <Pressable style={styles.circleButton} onPress={onPress}>
+        <MaterialIcons name="add" size={38} color="#25292e" />
+      </Pressable>
+    </View>
+  );
+}
