5d1c6b8281601ea078ed6bee15b62983dfd5b3c7
AR123456 <coloradoz99@gmail.com>
2026-03-28 11:07:32 -0400

    button in index

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,5 @@
 import { View, StyleSheet } from "react-native";
+import Button from "@/components/Button";
 import ImageViewer from "@/components/ImageViewer";
 
 const PlaceholderImage = require("../../assets/images/background-image.png");
@@ -7,6 +8,10 @@ export default function Index() {
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo"></Button>
+        <Button label="Use this photo"></Button>
       </View>
     </View>
   );
@@ -18,17 +23,11 @@ const styles = StyleSheet.create({
     backgroundColor: "#25292e",
   },
 
-  button: {
-    fontSize: 20,
-    textDecorationLine: "underline",
-    color: "#fff",
-  },
   imageContainer: {
     flex: 1,
   },
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
   },
 });
