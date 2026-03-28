a356533cae2bd824eeefdc2049e334dce0d2cadd
AR123456 <coloradoz99@gmail.com>
2026-03-28 11:04:37 -0400

    icons to tabs

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,65 +1,29 @@
-import { View, StyleSheet } from "react-native";
-import * as ImagePicker from "expo-image-picker";
-import { useState } from "react";
-
-import Button from "@/components/Button";
-import ImageViewer from "@/components/ImageViewer";
-
-const PlaceholderImage = require("@/assets/images/background-image.png");
+import { Link } from "expo-router";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
-  const [selectedImage, setSelectedImage] = useState<string | undefined>(
-    undefined,
-  );
-  const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
-
-  const pickImageAsync = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ["images"],
-      allowsEditing: true,
-      quality: 1,
-    });
-    if (!result.canceled) {
-      setSelectedImage(result.assets[0].uri);
-      setShowAppOptions(true);
-    } else {
-      alert("You did not select an image.");
-    }
-  };
   return (
     <View style={styles.container}>
-      <View style={styles.imageContainer}>
-        <ImageViewer
-          imgSource={PlaceholderImage}
-          selectedImage={selectedImage}
-        />
-      </View>
-      <View style={styles.footerContainer}>
-        <Button
-          theme="primary"
-          label="Choose a photo"
-          onPress={pickImageAsync}
-        />
-        <Button
-          label="Use this photo"
-          onPress={() => setShowAppOptions(true)}
-        />
-      </View>
+      <Text style={styles.text}>Hello World !</Text>
+      <Link href="/about" style={styles.button}>
+        Go to About Screen
+      </Link>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
     backgroundColor: "#25292e",
-    alignItems: "center",
   },
-  imageContainer: {
-    flex: 1,
+  text: {
+    color: "#fff",
   },
-  footerContainer: {
-    flex: 1 / 3,
-    alignItems: "center",
+  button: {
+    fontSize: 20,
+    textDecorationLine: "underline",
+    color: "#fff",
   },
 });
