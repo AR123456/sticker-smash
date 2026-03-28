4bb62ee855d2ad9eb79f96424b72e0e5b5fecd67
AR123456 <coloradoz99@gmail.com>
2026-03-28 11:06:27 -0400

    getting background image

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,30 +1,33 @@
-// import { Link } from "expo-router";
 import { Text, View, StyleSheet } from "react-native";
 import { Image } from "expo-image";
 const PlaceholderImage = require("../../assets/images/background-image.png");
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello World !</Text>
-      {/* <Link href="/about" style={styles.button}>
-        Go to About Screen
-      </Link> */}
+      <View style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image}></Image>
+      </View>
     </View>
   );
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#25292e",
   },
-  text: {
-    color: "#fff",
-  },
+
   button: {
     fontSize: 20,
     textDecorationLine: "underline",
     color: "#fff",
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
   },
 });
