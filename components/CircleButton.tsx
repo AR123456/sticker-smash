cda2ab4c0a4dea48c49751013d6ef24fe4b761c3
AR123456 <coloradoz99@gmail.com>
2026-03-31 07:04:26 -0400

    returning circle button view

--- components/CircleButton.tsx
+++ components/CircleButton.tsx
@@ -4,4 +4,14 @@ import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 type Props = {
   onPress: () => void;
 };
-export default function CircleButton({ onPress }: Props) {}
+export default function CircleButton({ onPress }: Props) {
+  return (
+    <View style={styles.circleButtonContainer}>
+      <Pressable>
+        <MaterialIcons></MaterialIcons>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
