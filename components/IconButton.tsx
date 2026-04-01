e73236d4f32b69f44e52c265ad86c2bf5513ff57
AR123456 <coloradoz99@gmail.com>
2026-04-01 08:34:16 -0400

    icon button in default

--- components/IconButton.tsx
+++ components/IconButton.tsx
@@ -1,11 +1,17 @@
 import { Text, Pressable, StyleSheet } from "react-native";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
-type Props={
-    icon:keyof typeof MaterialIcons.glyphMap;
-}
+type Props = {
+  icon: keyof typeof MaterialIcons.glyphMap;
+  label: string;
+  onPress: () => void;
+};
 
-
-export default function IconButton({icon,label,onPress}:Props) {
-    return()
+export default function IconButton({ icon, label, onPress }: Props) {
+  return (
+    <Pressable style={styles.iconButton} onPress={onPress}>
+      <MaterialIcons name={icon} size={24} color="#fff" />
+      <Text style={styles.iconButtonLabel}>{label}</Text>
+    </Pressable>
+  );
 }
