28b9826c4986965b504c950f109d401ded1f9757
AR123456 <coloradoz99@gmail.com>
2026-04-01 08:33:56 -0400

    types in icon button

--- components/IconButton.tsx
+++ components/IconButton.tsx
@@ -1,2 +1,11 @@
 import { Text, Pressable, StyleSheet } from "react-native";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+
+type Props={
+    icon:keyof typeof MaterialIcons.glyphMap;
+}
+
+
+export default function IconButton({icon,label,onPress}:Props) {
+    return()
+}
