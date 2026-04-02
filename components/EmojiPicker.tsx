4439314cf3fecfa277b10a59c5e4f384cc08416e
AR123456 <coloradoz99@gmail.com>
2026-04-02 08:36:21 -0400

    imports

--- components/EmojiPicker.tsx
+++ components/EmojiPicker.tsx
@@ -1,1 +1,8 @@
-type Props = {};
+import { PropsWithChildren } from "react";
+import { Modal, View, Text, Pressable, StyleSheet } from "react-native";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+
+type Props = PropsWithChildren<{
+  isVisible: boolean;
+  onClose: () => void;
+}>;
