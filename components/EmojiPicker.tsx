15343184e437bad7d1862ada8ffd926c197f51d3
AR123456 <coloradoz99@gmail.com>
2026-04-02 08:36:40 -0400

    return emoji picker

--- components/EmojiPicker.tsx
+++ components/EmojiPicker.tsx
@@ -1,8 +1,19 @@
 import { PropsWithChildren } from "react";
-import { Modal, View, Text, Pressable, StyleSheet } from "react-native";
+import {
+  Modal,
+  View,
+  Text,
+  Pressable,
+  StyleSheet,
+  registerCallableModule,
+} from "react-native";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
 type Props = PropsWithChildren<{
   isVisible: boolean;
   onClose: () => void;
 }>;
+
+export default function EmojiPicker({ isVisible, children, onClose }: Props) {
+  return <View></View>;
+}
