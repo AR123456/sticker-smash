e1a04e34a14bf715b675839357830bba3b4941da
AR123456 <coloradoz99@gmail.com>
2026-04-03 06:34:12 -0400

    title css

--- components/EmojiPicker.tsx
+++ components/EmojiPicker.tsx
@@ -25,16 +25,27 @@ export default function EmojiPicker({ isVisible, children, onClose }: Props) {
 }
 const styles = StyleSheet.create({
   modalContent: {
-    height: '25%',
-    width: '100%',
-    backgroundColor: '#25292e',
+    height: "25%",
+    width: "100%",
+    backgroundColor: "#25292e",
     borderTopRightRadius: 18,
     borderTopLeftRadius: 18,
-    position: 'absolute',
+    position: "absolute",
     bottom: 0,
   },
-  
-  titleContainer{},
-  title{},
 
+  titleContainer: {
+    height: "16%",
+    backgroundColor: "#464C55",
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+    paddingHorizontal: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  title: {
+    color: "#fff",
+    fontSize: 16,
+  },
 });
