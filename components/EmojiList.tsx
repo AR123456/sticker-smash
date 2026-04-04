f734615e4c621e19225f71d710c525026a18fecd
AR123456 <coloradoz99@gmail.com>
2026-04-04 08:06:09 -0400

    pass source and item to image

--- components/EmojiList.tsx
+++ components/EmojiList.tsx
@@ -28,16 +28,31 @@ export default function EmojiList({ onSelect, onCloseModal }: Props) {
       showsHorizontalScrollIndicator={Platform.OS === "web"}
       data={emoji}
       contentContainerStyle={styles.listContainer}
-      renderItem={({ items, index }) => (
+      renderItem={({ item, index }) => (
         <Pressable
           onPress={() => {
             onSelect(item);
             onCloseModal();
           }}
         >
-          <Image></Image>
+          <Image source={item} key={index} style={styles.image}></Image>
         </Pressable>
       )}
     ></FlatList>
   );
 }
+const styles = StyleSheet.create({
+  listContainer: {
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+    paddingHorizontal: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  image: {
+    width: 100,
+    height: 100,
+    marginRight: 20,
+  },
+});
