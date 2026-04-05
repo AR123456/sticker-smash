2c784a0787a171d78826f8ffb51c84586f564905
AR123456 <coloradoz99@gmail.com>
2026-04-05 05:12:21 -0400

    starting emojisticker

---
+++ components/EmojiSticker.tsx
@@ -0,0 +1,12 @@
+import { ImageSourcePropType, View } from "react-native";
+import { ImageRef } from "expo-image";
+
+export default class EmojiSticker extends Component {
+  render() {
+    return (
+      <View>
+        <Text>EmojiSticker</Text>
+      </View>
+    );
+  }
+}
