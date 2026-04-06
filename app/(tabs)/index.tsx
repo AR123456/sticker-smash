cc02ab716a94c5696b9f1642960491b97fe3c582
AR123456 <coloradoz99@gmail.com>
2026-04-06 07:56:10 -0400

    gesture handler to root view in index

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
 import { ImageSourcePropType, View, StyleSheet } from "react-native";
 import * as ImagePicker from "expo-image-picker";
-import { useState } from "react";
+import React, { useState } from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import Button from "@/components/Button";
 import ImageViewer from "@/components/ImageViewer";
@@ -49,7 +50,7 @@ export default function Index() {
     // we will implement this later
   };
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer
           imgSource={PlaceholderImage}
@@ -93,7 +94,7 @@ export default function Index() {
           onCloseModal={onModalClose}
         ></EmojiList>
       </EmojiPicker>
-    </View>
+    </GestureHandlerRootView>
   );
 }
 
