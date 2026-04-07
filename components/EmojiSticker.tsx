db7874bbbb0d0c06b8b6915f40508ee7b74eba85
AR123456 <coloradoz99@gmail.com>
2026-04-07 09:49:03 -0400

    gesture

--- components/EmojiSticker.tsx
+++ components/EmojiSticker.tsx
@@ -14,7 +14,6 @@ type Props = {
 export default function EmojiSticker({ imageSize, stickerSource }: Props) {
   const scaleImage = useSharedValue(imageSize);
   const translateX = useSharedValue(0);
-  const translateY = useSharedValue(0);
   const doubleTap = Gesture.Tap()
     .numberOfTaps(2)
     .onStart(() => {
@@ -30,13 +29,8 @@ export default function EmojiSticker({ imageSize, stickerSource }: Props) {
       height: withSpring(scaleImage.value),
     };
   });
-  const drag = Gesture.Pan().onChange((event) => {
-    translateX.value += event.changeX;
-    translateY.value += event.changeY;
-  });
-
   return (
-    <Animated.View style={{ top: -350 }}>
+    <View style={{ top: -350 }}>
       <GestureDetector gesture={doubleTap}>
         <Animated.Image
           source={stickerSource}
@@ -44,6 +38,6 @@ export default function EmojiSticker({ imageSize, stickerSource }: Props) {
           style={[imageStyle, { width: imageSize, height: imageSize }]}
         ></Animated.Image>
       </GestureDetector>
-    </Animated.View>
+    </View>
   );
 }
