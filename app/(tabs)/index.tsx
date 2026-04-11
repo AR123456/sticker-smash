411c8be081e05ff10220c737235b9d4551472ca3
AR123456 <coloradoz99@gmail.com>
2026-04-11 05:55:59 -0400

    using data dom to image

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -72,6 +72,15 @@ export default function Index() {
       }
     } else {
       try {
+        const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+          quality: 0.95,
+          width: 320,
+          height: 440,
+        });
+        let link = document.createElement("a");
+        link.download = "sticker-smash.jpeg";
+        link.href = dataUrl;
+        link.click();
       } catch (e) {
         console.log(e);
       }
